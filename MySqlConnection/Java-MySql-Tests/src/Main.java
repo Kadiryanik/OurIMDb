a85690df95ed2d3f2408ee8d2ed7271929ea82d3
Çağatay Avşar <aatay Avar>
2016-10-07 00:15:00 +0300

    yorum

--- MySqlConnection/Java-MySql-Tests/src/Main.java
+++ MySqlConnection/Java-MySql-Tests/src/Main.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-//cagatay
+//cagatay avsar
 public class Main {
 	public static void main(String[] args) throws Exception{
 		//Accessing driver from JAR file
